9dff3f01f528529b7f80c9903a632d54b8791e12
tmmcmasters <tmmcmasters@gmail.com>
2025-12-09 16:36:29 -0500

    No Cargo

--- Day One/main.rs
+++ Day One/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    
+    // I learned that rustc builds it and then we have to run the executable manually
+    let x = 5;
+    println!("the value of x is {}", x);
+
+    let x = 6;
+    println!("the value of x is {}", x);
 }
