63dd2b5bd2cebcf0068f3f464c41d775c4a469da
tmmcmasters <tmmcmasters@gmail.com>
2025-12-09 16:40:19 -0500

    Learning Cargo run

---
+++ Day One/hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9.d
@@ -0,0 +1,5 @@
+/Volumes/CrucialP3/codeProjects/polkadot/Day One/hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9.d: src/main.rs
+
+/Volumes/CrucialP3/codeProjects/polkadot/Day One/hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9: src/main.rs
+
+src/main.rs:
---
+++ Day One/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Volumes/CrucialP3/codeProjects/polkadot/Day\ One/hello_cargo/target/debug/hello_cargo: /Volumes/CrucialP3/codeProjects/polkadot/Day\ One/hello_cargo/src/main.rs
